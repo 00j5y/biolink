3e8add0c4402254437ecc77287b34ae96a8d51a0
00j5y <spmt.jay@gmail.com>
2026-02-24 17:31:57 +0100

    {CI} Premier commit

---
+++ components/Spotify.tsx
@@ -0,0 +1,11 @@
+export default function Spotify(){
+    return (
+        <div className="bg-black h-20 w-full ">
+            <div className="flex items-center justify-center h-full">
+                <button className="bg-white rounded-full h-10 w-10">
+                    
+                </button>
+            </div>
+        </div>
+    );
+}
